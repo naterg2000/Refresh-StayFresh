1bdfb58ca5bd515b53ba128174ff63fd8851fd20
naterg2000 <naterg2000@gmail.com>
2023-04-28 16:05:06 -0700

    Updated the mission statment vectors

---
+++ android/app/src/main/java/com/nathangoshay/refresh_stayfresh/MainActivity.java
@@ -0,0 +1,6 @@
+package com.nathangoshay.refresh_stayfresh;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/components/MyIcons.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:refresh_stayfresh/assets/vectors/Logo.svg';
+
+const refreshLogo = 'lib/assets/vectors/Logo.svg';
